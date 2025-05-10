9541e5a734a868890d5f80961b4fd3d02116af69
Project999x <105520683+Project999x@users.noreply.github.com>
2025-05-10 06:46:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,11 +17,11 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "7589673329:AAH2l8fAo9ODSeTd7i5s3ueB41gc8_7
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/25045d4a59a9efc54b89a-faf3b0f89f092a9e99.jpg')).split()
 
 
 # Admins & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()] # For Multiple Id Use One Space Between Each.
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1418213560').split()] # For Multiple Id Use One Space Between Each.
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]  # For Multiple Id Use One Space Between Each.
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
